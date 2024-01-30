b1597c23a1104aaf88534205123c30b7a510446f
Humayra Tahira Hussain <humayra.h@hotmail.com>
2024-01-30 15:46:53 +0000

    Added conditional to string

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -2,12 +2,12 @@
 onePlusone = "1 + 1 = 2"
 
 main = do
+    putStrLn(onePlusone)
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
     putStrLn "Welcome to Haskell. Would you like to begin the quiz?"
     yes <- getLine
-    putStrLn("Okay" ++ name ++ ", How can we run haskell on GitPod?")
-    answer <-getLine
-    putStrLn "Yes, that is correct!"
-    putStrLn(onePlusone)
+    if yes == "yes"
+        then putStrLn("Okay" ++ name ++ ", you're lucky there is no quiz!")
+        else putStrLn("quitting...")
