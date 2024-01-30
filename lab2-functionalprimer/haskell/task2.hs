36e54c46c0cefbaea792365d131d8f6c6b276729
Humayra Tahira Hussain <humayra.h@hotmail.com>
2024-01-30 15:10:09 +0000

    Added String arguments and got input from user

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -2,3 +2,8 @@ main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    putStrLn "Welcome to Haskell. Would you like to begin the quiz?"
+    yes <- getLine
+    putStrLn("Okay" ++ name ++ ", How can we run haskell on GitPod?")
+    answer <-getLine
+    putStrLn "Yes, that is correct!"
